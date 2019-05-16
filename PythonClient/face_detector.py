90ac7c5a881fc143d20161a27f0562bbdfe805de
scarlet <1996scarlet@gmail.com>
2019-05-17 06:36:38 +0800

    updated retinaface

--- PythonClient/face_detector.py
+++ PythonClient/face_detector.py
@@ -5,75 +5,44 @@ import numpy as np
 import mxnet as mx
 import cv2
 import time
-from mtcnn_detector import MtcnnDetector
+from retinaface import RetinaFace
+from face_preprocess import preprocess
 
-import face_preprocess
 
 class DetectorModel:
     def __init__(self, args):
-        ctx = mx.cpu() if args.gpu == -1 else mx.gpu(args.gpu)
-        mtcnn_path = os.path.join(os.path.dirname(__file__), './zoo/mtcnn-model')
-
+        self.detector = RetinaFace(args.retina_model, 0, args.gpu, 'net3')
+        self.threshold = args.threshold
+        self.scales = args.scales
         self.max_face_number = args.max_face_number
         self.counter = 0
-        self.detector = MtcnnDetector(model_folder=mtcnn_path, ctx=ctx, num_worker=1,
-                                      minsize=args.mtcnn_minsize, factor=args.mtcnn_factor,
-                                      accurate_landmark=True, threshold=args.mtcnn_threshold)
+        self.image_size = args.image_size
 
     def save_image(self, image):
-        cv2.imwrite('./Temp/{}-{}.jpg'.format(time.time(), self.counter), image)
+        cv2.imwrite('./Temp/{}-{}.jpg'.format(time.time(), self.counter),
+                    image)
         self.counter += 1
 
-    def get_all_boxes(self, face_img, save_img=False):
-        face_num = self.max_face_number
-        ret = self.detector.detect_face(face_img, det_type=0)
-        if ret is None:
-            return []
+    def get_all_boxes(self, img, save_img=False):
+        faces, landmarks = self.detector.detect(img,
+                                                self.threshold,
+                                                scales=self.scales)
 
-        bbox, points = ret
-
-        sorted_index = bbox[:, 0].argsort()
-        bbox = bbox[sorted_index]
-        points = points[sorted_index]
+        sorted_index = faces[:, 0].argsort()
+        faces = faces[sorted_index]
+        landmarks = landmarks[sorted_index]
 
         aligned = []
-        for index in range(0, len(bbox[:face_num])):
-            item_bbox = bbox[index, 0:4]
-            item_points = points[index, :].reshape((2, 5)).T
-            nimg = face_preprocess.preprocess(
-                face_img, item_bbox, item_points, image_size='112,112')
+        # print('find', faces.shape[0], 'faces')
+        for i in range(len(faces[:self.max_face_number])):
+            nimg = preprocess(img,
+                              faces[i],
+                              landmarks[i],
+                              image_size=self.image_size)
 
             if save_img:
-                save_image(nimg)
+                self.save_image(nimg)
 
             aligned.append(nimg)
 
-        return zip(aligned, bbox)
-
-    def get_boxes_and_landmarks(self, face_img, save_img=False):
-        face_num = self.max_face_number
-        ret = self.detector.detect_face(face_img, det_type=0)
-        if ret is None:
-            return []
-
-        bbox, points = ret
-
-        sorted_index = bbox[:, 0].argsort()
-        bbox = bbox[sorted_index]
-        points = points[sorted_index]
-
-        aligned = []
-        pois = []
-        for index in range(0, len(bbox[:face_num])):
-            item_bbox = bbox[index, 0:4]
-            item_points = points[index, :].reshape((2, 5)).T
-            nimg = face_preprocess.preprocess(
-                face_img, item_bbox, item_points, image_size='112,112')
-
-            if save_img:
-                save_image(nimg)
-
-            aligned.append(nimg)
-            pois.append(item_points)
-
-        return zip(aligned, bbox, pois)
+        return zip(aligned, faces)
--- PythonClient/vtuber_usb_camera.py
+++ PythonClient/vtuber_usb_camera.py
@@ -27,6 +27,10 @@ async def upload_loop(url="http://127.0.0.1:6789"):
             euler_angle, shape, leftEAR, rightEAR = result_queue.get_nowait()
             result_string = {'X': euler_angle[0, 0], 'Y': euler_angle[1, 0], 'Z': euler_angle[2, 0],
                              'shape': shape.tolist(), 'leftEAR': leftEAR, 'rightEAR': rightEAR}
+
+            # print({'X': euler_angle[0, 0], 'Y': euler_angle[1, 0], 'Z': euler_angle[2, 0],
+            #        'leftEAR': leftEAR, 'rightEAR': rightEAR})
+
             await sio.emit('result_data', result_string, namespace='/sakuya')
         except Exception as e:
             print(e)
@@ -48,6 +52,7 @@ async def camera_loop(preload):
 
     with mss.mss() as sct:
         monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+        # monitor = {"top": 0, "left": 0, "width": 192, "height": 108}
         while True:
             start_time = loop.time()
 
@@ -72,17 +77,19 @@ async def detection_loop(preload):
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    video_capture = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 540)
 
     # =================== ETERNAL LOOP ====================
     while True:
         start_time = loop.time()
-
+        head_frame = cap.read()[1]
         try:
-            ret, head_frame = video_capture.read()[1]
-
-            for img, box, points in detector.get_boxes_and_landmarks(head_frame):
-                box = box.astype(int)
+            for img, box in detector.get_all_boxes(head_frame):
+                box = box.astype(np.int)
+                cv2.rectangle(head_frame, (box[0], box[1]), (box[2], int(box[3])),
+                              [255, 255, 0], 2)
                 # nimg = cv2.resize(
                 # head_frame[box[1]:box[3], box[0]:box[2]], (112, 112))
                 shape = predictor(img, dlib.rectangle(0, 0, 112, 112))
@@ -92,12 +99,12 @@ async def detection_loop(preload):
                 shape = face_utils.shape_to_np(shape)
                 reprojectdst, euler_angle = get_head_pose(shape)
 
-                for (x, y) in shape:
-                    cv2.circle(head_frame, (x, y), 2, (0, 0, 255), -1)
+                # for (x, y) in shape:
+                #     cv2.circle(head_frame, (x, y), 2, (0, 0, 255), -1)
 
-                for start, end in line_pairs:
-                    cv2.line(
-                        head_frame, reprojectdst[start], reprojectdst[end], (0, 0, 255))
+                # for start, end in line_pairs:
+                #     cv2.line(
+                #         head_frame, reprojectdst[start], reprojectdst[end], (0, 0, 255))
 
                 # =================== EYES ====================
                 shape = shape * 10
@@ -107,13 +114,14 @@ async def detection_loop(preload):
 
                 leftEAR = eye_aspect_ratio(leftEye)
                 rightEAR = eye_aspect_ratio(rightEye)
-
-                head_frame = np.zeros([540, 960, 3])
-
+                # head_frame = np.zeros([540, 960, 3])
                 result_queue.put((euler_angle, shape, leftEAR, rightEAR))
         except Exception as e:
-            print(e)
+            # print(e)
             pass
+
+        # cv2.imshow('result', head_frame)
+        # cv2.waitKey(1)
 
         print(colored(loop.time()-start_time, 'red'), flush=True)
 
@@ -125,10 +133,7 @@ async def detection_loop(preload):
 # =================== ARGS ====================
 os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
 args = start_up_init()
-args.mtcnn_minsize = 225
-args.mtcnn_factor = 0.1
-args.mtcnn_threshold = [0.5, 0.6, 0.7]
-args.face_landmark_path = './zoo/model-landmarks/shape_predictor_68_face_landmarks.dat'
+args.face_landmark_path = './model/landmarks.dat'
 
 # =================== INIT ====================
 frame_queue = Queue(maxsize=args.max_frame_rate)
